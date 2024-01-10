78ca359b45cfa15a7889395f3d55dfc6e45e48cb
Lsy <1228357237@qq.com>
2024-01-10 13:47:02 +0800

    first commit

---
+++ app/src/main/java/com/lsy/composewidget/widget/WidgetModel.kt
@@ -0,0 +1,23 @@
+package com.lsy.composewidget.widget
+
+/**
+ * Title :
+ * Author: Lsy
+ * Date: 2024/1/4 17:07
+ * Version: 1
+ * Description:
+ * History:
+ * <author> <time> <version> <desc>
+ * 作者姓名 修改时间 版本号 描述
+ */
+sealed class WidgetState {
+    data class InitState(val initName: String = "") : WidgetState()
+    object DisableState : WidgetState()
+    object EnableState : WidgetState()
+}
+
+sealed class WidgetSpec {
+    object Large : WidgetSpec()
+    object Medium : WidgetSpec()
+    object Small : WidgetSpec()
+}
